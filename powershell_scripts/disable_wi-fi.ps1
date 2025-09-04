14e79f9483a4b725954fca21a2c2a1e90091c2e7
Reed Waller <reed@blacklight.net>
2025-09-03 17:19:46 -0700

    Delete powershell_scripts directory

--- powershell_scripts/disable_wi-fi.ps1
+++
@@ -1,9 +0,0 @@
-# This script disables Wi-Fi
-
-Disable-NetAdapter -Name "Wi-Fi" -Confirm:$false
-
-Write-Host "Successfully disabled Wi-Fi."
-
-# Read-Host # Uncomment when testing, prevents the script window from closing so you can review the output
-
-# End.
--- powershell_scripts/enable_wi-fi.ps1
+++
@@ -1,9 +0,0 @@
-# This script enables Wi-Fi
-
-Enable-NetAdapter -Name "Wi-Fi" -Confirm:$false
-
-Write-Host "Successfully enabled Wi-Fi."
-
-# Read-Host # Uncomment when testing, prevents the script window from closing so you can review the output
-
-# End.

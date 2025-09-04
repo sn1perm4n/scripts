d3c52e1e76af09ecc473d0ae8d91321cb10daabd
Reed Waller <reed@blacklight.net>
2025-09-03 17:01:15 -0700

    Delete linux_scripts directory

--- linux_scripts/deleteFilesOlderThan6Months.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script deletes files older than 182 days from /*/*/*/*.
-# This script resides in /usr/local/bin and is run daily at x AM via cronjob.
-
-find /*/*/*/* -mtime +182 -exec rm -rf '{}' \;
-
-# End.
--- linux_scripts/synology_data_scrubbing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# RAID Data Scrubbing
-
-/bin/echo check > /sys/block/md0/md/sync_action
-/bin/echo check > /sys/block/md1/md/sync_action
-/bin/echo check > /sys/block/md2/md/sync_action
-
-# End.

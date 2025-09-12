51cd1b7377cce99c6dd8527a07407a9ef09399ca
Reed Waller <reed@blacklight.net>
2025-09-11 18:32:46 -0700

    Delete powershell_scripts directory

--- powershell_scripts/disable_bluetooth.ps1
+++
@@ -1,9 +0,0 @@
-# This script disables Bluetooth
-
-Get-PnpDevice -Class Bluetooth | Disable-PnpDevice -Confirm:$false
-
-Write-Host "Successfully disabled Bluetooth."
-
-# Read-Host # Uncomment when testing, prevents the script window from closing so you can review the output
-
-# End.

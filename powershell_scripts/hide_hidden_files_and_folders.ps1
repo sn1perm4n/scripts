d793d328e0c1658e08c212c0af75912ffcaf096e
Reed Waller <reed@blacklight.net>
2026-01-14 00:58:37 -0800

    Add -Type DWord and -Force to Set-ItemProperty

---
+++ powershell_scripts/hide_hidden_files_and_folders.ps1
@@ -0,0 +1,13 @@
+# This script hides hidden files and folders by making Registry changes and refreshing File Explorer to make the changes active
+
+$registryPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty -Path $registryPath -Name Hidden -Type DWord -Value 0 -Force
+Set-ItemProperty -Path $registryPath -Name ShowSuperHidden -Type DWord -Value 0 -Force
+$shell = New-Object -ComObject Shell.Application
+$shell.Windows() | ForEach-Object { $_.Refresh() }
+
+Write-Host "Successfully hid 'Hidden Files and Folders'."
+
+# Read-Host # Uncomment when testing, prevents the script window from closing so you can review the output
+
+# End.

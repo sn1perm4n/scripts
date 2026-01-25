a83bd76d3c1db13cd8327721bb28b612a972918e
Reed Waller <reed@blacklight.net>
2026-01-25 15:31:31 -0800

    Initial commit

---
+++ powershell_scripts/enable_dark_mode.ps1
@@ -0,0 +1,20 @@
+# This script enables dark mode in Windows 10 and 11 (activation not required)
+
+# Enable Dark Mode for Windows 10 and 11 (Apps + System)
+$regPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize'
+
+try {
+	if (-not (Test-Path $regPath)) {
+		New-Item -Path $regPath -Force | Out-Null
+	}
+
+	Set-ItemProperty -Path $regPath -Name "AppsUseLightTheme" -Type DWord -Value 0 -Force
+	Set-ItemProperty -Path $regPath -Name "SystemUsesLightTheme" -Type DWord -Value 0 -Force
+
+	Write-Host "`nDark mode successfully enabled." -ForegroundColor Green
+}
+catch {
+	Write-Host "Failed to enable dark mode: $($_.Exception.Message)." -ForegroundColor Red
+}
+
+# End.

5f00fc49bf6fc769a15fd1f5fc1922bceb084f5b
Reed Waller <reed@blacklight.net>
2026-04-04 01:45:58 -0700

    Added GitHub header comment, simplified admin check,  comment adjustments

--- powershell_scripts/disable_realtek_1gb_2.5gb_various_settings.ps1
+++ powershell_scripts/disable_realtek_1gb_2.5gb_various_settings.ps1
@@ -1,16 +1,13 @@
-﻿# This script disables various settings on Realtek 1Gb and 2.5Gb Ethernet cards that negatively impact performance (especially for gaming). The name of your Ethernet adapter may differ (i.e. on one of my systems it's "Local Area Connection") so confirm first with the following PowerShell command (if you don't do this you'll see a bunch of red errors): Get-NetAdapter
+﻿# GitHub repository (Reed Waller): https://github.com/sn1perm4n/scripts/tree/main/powershell_scripts
+# This script disables various settings on Realtek 1Gb and 2.5Gb Ethernet cards that negatively impact performance (especially for gaming). The name of your Ethernet adapter may differ (i.e. on one of my systems it's "Local Area Connection") so confirm first with the following PowerShell command (if you don't do this you'll see a bunch of red errors): Get-NetAdapter
 # I recommend that you confirm your Realtek NIC driver is up-to-date via your motherboard manufacturer's support website (or better yet your motherboard manufacturer's software, i.e. MSI Center, Armoury Crate, etc.). You will need to disable and re-enable your network adapter (or just restart your PC) for the below changes to take effect.
 # I also recommend re-running this script every time you update your Realtek Ethernet driver as there's a chance the settings could be reset to their default values.
-# Lastly, it would be a good idea to confirm the below settings exist for your NIC by running the following PowerShell command and comparing the output to this script: Get-NetAdapterAdvancedProperty -Name "Ethernet" | Sort-Object "DisplayName"
+# Lastly, it would be a good idea to confirm the below settings exist for your NIC by running the following PowerShell command and comparing the output to this script:
+# Get-NetAdapterAdvancedProperty -Name "Ethernet" | Sort-Object "DisplayName"
+
 # NOTE: These Registry settings are located in one of the subfolders located here: HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4D36E972-E325-11CE-BFC1-08002bE10318}
 
-# Ensure script runs as Administrator
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-	Write-Host "Please run this script as Administrator." -ForegroundColor Red
-	Write-Host "Press any key to exit..."
-	$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
-	exit
-}
+#Requires -RunAsAdministrator
 
 # Disable Advanced EEE (Energy-Efficient Ethernet)
 Set-NetAdapterAdvancedProperty -Name "Ethernet" -RegistryKeyword "AdvancedEEE" -RegistryValue 0

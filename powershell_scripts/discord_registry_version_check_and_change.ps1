ac29e43ae0ab577e8ca8ff77734f9038fd21b77c
Reed Waller <reed@blacklight.net>
2026-04-05 01:45:34 -0700

    GitHub header comment typo fixed and a newline added
    Nothing worth mentioning here

--- powershell_scripts/discord_registry_version_check_and_change.ps1
+++ powershell_scripts/discord_registry_version_check_and_change.ps1
@@ -1,6 +1,7 @@
-﻿# Github repository (Reed Waller): https://github.com/sn1perm4n/scripts/tree/main/powershell_scripts
+﻿# GitHub repository (Reed Waller): https://github.com/sn1perm4n/scripts/tree/main/powershell_scripts
 # Script to query and change the Discord DisplayVersion Registry value based on user input. Check what the version number says within the Discord's settings and then change it match within the Registry.
 # Reads (and changes) this value: HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Discord\DisplayVersion
+
 # NOTE: This script is no longer relevant as it appears Discord FINALLY fixed the bug in question. The entire reason I wrote this script was because the Discord installer/updater wasn't updating its own version number in the Windows registry, which then made winget (and likely other software update checkers) think it was always out of date. I'm going to keep this script on Github for learning purposes.
 
 # Registry path
